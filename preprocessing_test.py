ae7f91f93357baf4b826e30c6fb2530af2243a73
raoul <raoul.schorer@gmail.com>
2015-02-15 04:18:37 +0100

    updated tests

--- preprocessing_test.py
+++ preprocessing_test.py
@@ -6,6 +6,12 @@ import unittest, os
 
 class TestImagePreprocessing(unittest.TestCase):
 
+    def tearDown(self):
+        try:
+            os.remove('./source_test_images/gray_example.jpg')
+        except:
+            pass
+        
     def test_image_is_returned(self):
         preprocessing.preprocess('./source_test_images/example.jpg',
                                  './source_test_images', 'example.jpg')
